d3e8c3990791739ea534b6bd83100b272847ed99
mohsin <mohsinsikder895@gmail.com>
2024-03-12 21:41:38 +0600

    fetch data from tmdb api in the home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,24 @@
+import Results from "@/components/Results";
 
-export default function Home() {
-  return (
- <div>Home</div>
+const API_KEY=process.env.API_KEY
+
+export default async function Home({searchParams}) {
+  const genre = searchParams.genre || 'fetchTranding';
+  const res = await fetch(
+    `https://api.themoviedb.org/3${
+      genre === 'fetchTopRated' ? '/movie/top_rated' : '/trending/all/week'
+    }?api_key=${API_KEY}&language=en-US&page=1`
   );
+  const data=await res.json();
+  if(!res.ok){
+    throw new Error('Failed to fetch data')
+  }
+  const results =data.results;
+  // console.log(results);
+
+  
+  return <div>
+    <Results  results={results}/>
+  </div>
+ 
 }
