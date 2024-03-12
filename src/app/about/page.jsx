3280bab5e3d5f05b5446e78c8335b59bf5c91c17
mohsin <mohsinsikder895@gmail.com>
2024-03-12 18:45:46 +0600

    about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,45 @@
+
+export default function pages() {
+  return (
+    <div className=" max-w-6xl mx-auto p-3 space-y-4">
+    <h1 className=" text-2xl font-medium text-amber-600">About</h1>
+    <p>What is Next.js?
+Next.js is a React framework for building full-stack web applications. You use React Components to build user interfaces, and Next.js for additional features and optimizations.
+
+Under the hood, Next.js also abstracts and automatically configures tooling needed for React, like bundling, compiling, and more. This allows you to focus on building your application instead of spending time with configuration.
+
+Whether  an individual developer or part of a larger team, Next.js can help you build interactive, dynamic, and fast React applications.
+
+Main Features
+Some of the main Next.js features include:
+
+Feature	Description
+Routing	A file-system based router built on top of Server Components that supports layouts, nested routing, loading states, error handling, and more.
+Rendering	Client-side and Server-side Rendering with Client and Server Components. Further optimized with Static and Dynamic Rendering on the server with Next.js. Streaming on Edge and Node.js runtimes.
+Data Fetching	Simplified data fetching with async/await in Server Components, and an extended fetch API for request memoization, data caching and revalidation.
+Styling	Support for your preferred styling methods, including CSS Modules, Tailwind CSS, and CSS-in-JS
+Optimizations	Image, Fonts, and Script Optimizations to improve  Core Web Vitals and User Experience.
+TypeScript	Improved support for TypeScript, with better type checking and more efficient compilation, as well as custom TypeScript Plugin and type checker.</p>
+<p>
+What is Next.js?
+Next.js is a React framework for building full-stack web . You use React Components to build user interfaces, and Next.js for additional features and optimizations.
+
+Under the hood, Next.js also abstracts and automatically configures tooling needed for React, like bundling, compiling, and more. This allows you to focus on building your application instead of spending time with configuration.
+
+Whether  an individual developer or part of a larger team, Next.js can help you build interactive, dynamic, and fast React applications.
+
+Main Features
+Some of the main Next.js features include:
+
+Feature	Description
+Routing	A file-system based router built on top of Server Components that supports layouts, nested routing, loading states, error handling, and more.
+Rendering	Client-side and Server-side Rendering with Client and Server Components. Further optimized with Static and Dynamic Rendering on the server with Next.js. Streaming on Edge and Node.js runtimes.
+Data Fetching	Simplified data fetching with async/await in Server Components, and an extended fetch API for request memoization, data caching and revalidation.
+Styling	Support for your preferred styling methods, including CSS Modules, Tailwind CSS, and CSS-in-JS
+Optimizations	Image, Fonts, and Script Optimizations to improve your  Core Web Vitals and User Experience.
+TypeScript	Improved support for TypeScript, with better type checking and more efficient compilation, as well as custom TypeScript Plugin and type checker.
+</p>
+      
+    </div>
+  )
+}
