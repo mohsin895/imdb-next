16fdc2daa8d8d3a30efde5a0d13b9c3c30a1a06c
mohsin <mohsinsikder895@gmail.com>
2024-03-13 22:04:00 +0600

    add movie page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,7 +6,7 @@ export default function Card({result}) {
     <div className="group cursor-pointer
      sm:hover:shadow-slate-400 sm:shadow-md rounded-lg
       sm:border-slate-400 sm:m-2 transation-shadow duration-200  ">
- <Link href={`/movue/${result.id}`}>
+ <Link href={`/movie/${result.id}`}>
 <Image src={`https://image.tmdb.org/t/p/original
 /${result.backdrop_path || result.poster_path}`}
 width={500} height={300}
